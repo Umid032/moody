4b13bc1ea11f3870fbc8b8abb2da32beb15e5531
Umid <157285698+Umid032@users.noreply.github.com>
2024-04-28 17:59:50 +0500

    Add files via upload

--- src/context/index.js
+++ src/context/index.js
@@ -1,31 +1,29 @@
-import { createContext, useState, useEffect } from 'react';
-import axios from 'axios';
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
-  const APIUrl = 'http://localhost:9000';
-  const [Data, setData] = useState([]);
+    const APIUrl = "http://localhost:9000";
+    const [Data, setData] = useState([]);
 
-  useEffect(() => {
-    try {
-      axios
-        .get(`${APIUrl}/products`)
-        .then((response) => {
-          const data = response.data;
-          setData(() => (data && data.length ? [...data] : []));
-        })
-        .catch((error) => console.error(error));
-    } catch (error) {
-      alert(error?.message);
-      console.warn(error);
+    useEffect(() => {
+        try {
+            axios.get(`${APIUrl}/products`).then(response => {
+                const data = response.data;
+                setData(() => data && data.length ? [...data] : [])
+            }).catch(error => console.error(error))
+        } catch (error) {
+            alert(error?.message);
+            console.warn(error);
+        }
+    }, []);
+
+    const values = {
+        APIUrl, Data, setData
     }
-  }, []);
 
-  const values = {
-    APIUrl,
-    Data,
-    setData,
-  };
-  return <Context.Provider value={values}>{children}</Context.Provider>;
-};
+    return <Context.Provider value={values}>
+        {children}
+    </Context.Provider>
+}
